8e4da0d97face8fcfaf7a4c97ce1151157534f48
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2019-04-12 17:46:06 +0800

    edit--menu--icon--multipage

--- assets/js/style.js
+++ assets/js/style.js
@@ -6,11 +6,29 @@ $(function(){
 	// console.log(url.slice(url.lastIndexOf('/') + 1));
 	// console.log(folders[folders.length-2]);
 	// console.log(hash);
+	function pageScroll(){
+		$('a.page-scroll[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: (target.offset().top - 30)
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	}
+	pageScroll();
 	$("aside").load("aside.html", function(){
 		$('a[href*="' + (url.slice(url.lastIndexOf('/') + 1)) + '"]').parent().addClass('now');
+		pageScroll();
 	});
 	$("footer").load("footer.html");
 	$("span.detail").each(function(){
 		$(this).children().text($(this).attr("data-val"));
 	});
+}, function(){
+	"use strict";
 });

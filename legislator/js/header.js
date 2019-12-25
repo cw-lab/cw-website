c0e6ee5cac2ffcd3179f1d66f324d66b22905848
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2019-12-25 16:47:11 +0800

    legislator

--- legislator/js/header.js
+++ legislator/js/header.js
@@ -1,27 +1,29 @@
 ﻿$(function() {
-    var iScrollPos = 0,
-        height = $(window).height();
-    $('.hamburger').click(function() {
-        $('body, nav').toggleClass("menu__opened");
-        $(this).toggleClass("active");
-        $('.menu__block').toggleClass("default active");
+    $(".navigation").load("navigation.html", function() {
+        var iScrollPos = 0,
+            height = $(window).height();
+        $('.hamburger').click(function() {
+            $('body, nav').toggleClass("menu__opened");
+            $(this).toggleClass("active");
+            $('.menu__block').toggleClass("default active");
+        });
+        $(window).scroll(function() {
+            // 往上滾動出現<nav>
+            var iCurScrollPos = $(this).scrollTop();
+            if (iCurScrollPos < iScrollPos) {
+                $("nav").addClass("scrollUp");
+            } else {
+                $("nav").removeClass("scrollUp");
+            }
+            iScrollPos = iCurScrollPos;
+            if ($("nav").offset().top == 0) {
+                $("nav").removeClass("scrollUp");
+            }
+            if ($("nav").offset().top > height) {
+                $("nav").removeClass("firstscreen");
+            } else {
+                $("nav").addClass("firstscreen");
+            }
+        })
     });
-    $(window).scroll(function() {
-        // 往上滾動出現<nav>
-        var iCurScrollPos = $(this).scrollTop();
-        if (iCurScrollPos < iScrollPos) {
-            $("nav").addClass("scrollUp");
-        } else {
-            $("nav").removeClass("scrollUp");
-        }
-        iScrollPos = iCurScrollPos;
-        if ($("nav").offset().top == 0) {
-            $("nav").removeClass("scrollUp");
-        }
-        if ($("nav").offset().top > height) {
-            $("nav").removeClass("firstscreen");
-        } else {
-            $("nav").addClass("firstscreen");
-        }
-    })
 });

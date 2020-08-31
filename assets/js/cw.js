cb0c835d396ec0bae62f20e357e964bbef86834a
yutingh <yutingh@cw.com.tw>
2020-08-31 16:31:23 +0800

    新增loader、CLS指標處理、google font使用js載入

--- assets/js/cw.js
+++ assets/js/cw.js
@@ -34,6 +34,18 @@ function imgZoom() {
 }
 imgZoom();
 
+WebFontConfig = {
+    google: { families: [ 'Noto+Sans+TC:100,300,400,500,700','Roboto:100,300,400,500,700' ] }
+};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
+
 $(function() {
     var width = $(window).width(),
         height = $(window).height(),

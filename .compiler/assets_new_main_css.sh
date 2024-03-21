f415b2a54f0ab4089374ee3b43cc3a397dbdae11
TitanLin <titanlin@cw.com.tw>
2024-03-21 11:19:16 +0800

    chore: compiler updated.

---
+++ .compiler/assets_new_main_css.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sass --style=compressed --no-source-map ../assets_new/sass/main.scss ./build/main.css
--- cw_css.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sass --style=compressed --no-source-map ./assets/sass/cw.scss ./.cw/cw.css

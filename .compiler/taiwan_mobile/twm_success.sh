796751af384d9e42832f4dbb0cd036e2a037ea20
TitanLin <titanlin@cw.com.tw>
2024-11-11 16:05:29 +0800

    fix: dictionary icon issue

--- .compiler/taiwan_mobile/twm_success.sh
+++ .compiler/taiwan_mobile/twm_success.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sass --style=compressed --no-source-map ../assets/sass/twm_success.scss ./build/twm_success.css
+sass --style=compressed --no-source-map ../../assets/sass/twm_success.scss ./build/twm_success.css
